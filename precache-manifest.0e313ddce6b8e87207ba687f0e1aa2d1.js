65ddfe64a019f1f5281b51fd2d37c92924366522
DungPV <dungpv@bravebits.vn>
2020-05-28 16:22:40 +0700

    Updates

---
+++ precache-manifest.0e313ddce6b8e87207ba687f0e1aa2d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62466a30a2b64fe18d19cd72b860d612",
+    "url": "/cat-monsters/index.html"
+  },
+  {
+    "revision": "393f0949a4ced5ceb662",
+    "url": "/cat-monsters/static/css/main.5d4bd88a.chunk.css"
+  },
+  {
+    "revision": "d9168bbbd8fa150aa8b9",
+    "url": "/cat-monsters/static/js/2.0a21fc5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cat-monsters/static/js/2.0a21fc5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "393f0949a4ced5ceb662",
+    "url": "/cat-monsters/static/js/main.1b2e2e36.chunk.js"
+  },
+  {
+    "revision": "cb39c877f45f87a5c11c",
+    "url": "/cat-monsters/static/js/runtime-main.39c01307.js"
+  }
+]);
